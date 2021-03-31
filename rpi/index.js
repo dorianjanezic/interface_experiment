b059cb26446452c1ccd451905d52c949cd475ffc
lukastanicic <luka.stanicic@gmail.com>
2021-03-31 21:58:17 +0200

    Updated index.js

--- rpi/index.js
+++ rpi/index.js
@@ -1,36 +1,7 @@
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
-var fs = require('fs'); //require filesystem module
-var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+const aedes = require('aedes')();
+const server = require('net').createServer(aedes.handle);
+const port = 1883;
 
-http.listen(8080); //listen to port 8080
-
-function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-}
-
-io.sockets.on('connection', function (socket) {// WebSocket Connection
-  var lightvalue = 0; //static variable for current status
-  pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton
-    if (err) { //if an error
-      console.error('There was an error', err); //output error message to console
-      return;
-    }
-    lightvalue = value;
-    socket.emit('light', lightvalue); //send button status to client
-  });
-  socket.on('light', function(data) { //get light switch status from client
-    lightvalue = data;
-  });
-});
-
-process.on('SIGINT', function () { //on ctrl+c
-  process.exit(); //exit completely
+server.listen(port, function () {
+  console.log('server started and listening on port ', port);
 });

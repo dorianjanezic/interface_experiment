b80e82115dfd2f87c5d35faebb68f88a815141e9
lukastanicic <luka.stanicic@gmail.com>
2021-04-10 12:41:16 +0200

    Updated code, working with arduino nano iot lsm6ds33 sensor

--- arduino/Measure_angle_Arduino_MPU6050/Measure_angle_Arduino_MPU6050.ino
+++ arduino/Measure_angle_Arduino_MPU6050/Measure_angle_Arduino_MPU6050.ino
@@ -1,21 +1,54 @@
-///////////////////////////////////////////////////////////////////////////////////////
-//THIS IS A DEMO SOFTWARE JUST FOR EXPERIMENT PURPOER IN A NONCOMERTIAL ACTIVITY
-//Version: 1.0 (AUG, 2016)
+#include <Arduino.h>
+#line 1 "C:\\Users\\lukas\\Documents\\git\\interface_experiment\\arduino\\Measure_angle_Arduino_MPU6050\\Measure_angle_Arduino_MPU6050.ino"
 
-//Gyro - Arduino UNO R3
-//VCC  -  5V
-//GND  -  GND
-//SDA  -  A4
-//SCL  -  A5
-//INT - port-2
 
 #include <Wire.h>
+#include <MQTT.h>
+#include <WiFiNINA.h>
+#include "Arduino_LSM6DS3.h"
+
+
+ char ssid[] = "T-2_4295d1";
+ char pass[] = "INNBOX2729000677";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+#line 17 "C:\\Users\\lukas\\Documents\\git\\interface_experiment\\arduino\\Measure_angle_Arduino_MPU6050\\Measure_angle_Arduino_MPU6050.ino"
+void connect();
+#line 49 "C:\\Users\\lukas\\Documents\\git\\interface_experiment\\arduino\\Measure_angle_Arduino_MPU6050\\Measure_angle_Arduino_MPU6050.ino"
+void messageReceived(String &topic, String &payload);
+#line 52 "C:\\Users\\lukas\\Documents\\git\\interface_experiment\\arduino\\Measure_angle_Arduino_MPU6050\\Measure_angle_Arduino_MPU6050.ino"
+void setup();
+#line 85 "C:\\Users\\lukas\\Documents\\git\\interface_experiment\\arduino\\Measure_angle_Arduino_MPU6050\\Measure_angle_Arduino_MPU6050.ino"
+void loop();
+#line 17 "C:\\Users\\lukas\\Documents\\git\\interface_experiment\\arduino\\Measure_angle_Arduino_MPU6050\\Measure_angle_Arduino_MPU6050.ino"
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    Serial.print(WiFi.status());
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "public", "public")) {
+    Serial.print("/");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("hello");
+}
 //Declaring some global variables
-int gyro_x, gyro_y, gyro_z;
+float gyro_x, gyro_y, gyro_z;
 long gyro_x_cal, gyro_y_cal, gyro_z_cal;
 boolean set_gyro_angles;
 
-long acc_x, acc_y, acc_z, acc_total_vector;
+float acc_x, acc_y, acc_z, acc_total_vector;
 float angle_roll_acc, angle_pitch_acc;
 
 float angle_pitch, angle_roll;
@@ -24,12 +57,22 @@ float angle_pitch_output, angle_roll_output;
 
 long loop_timer;
 int temp;
-
+void messageReceived(String &topic, String &payload) {
+  Serial.println(topic + ": " + payload);
+}
 void setup() {
-  Wire.begin();                                                        //Start I2C as master
-  setup_mpu_6050_registers();                                          //Setup the registers of the MPU-6050 
+
+     // start the IMU:
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU");
+    // stop here if you can't access the IMU:
+    while (true);
+  }
+  
   for (int cal_int = 0; cal_int < 1000 ; cal_int ++){                  //Read the raw acc and gyro data from the MPU-6050 for 1000 times
-    read_mpu_6050_data();                                             
+      // if both accelerometer and gyrometer are ready to be read:
+    // read accelerometer and gyrometer:
+    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);                                           
     gyro_x_cal += gyro_x;                                              //Add the gyro x offset to the gyro_x_cal variable
     gyro_y_cal += gyro_y;                                              //Add the gyro y offset to the gyro_y_cal variable
     gyro_z_cal += gyro_z;                                              //Add the gyro z offset to the gyro_z_cal variable
@@ -41,27 +84,36 @@ void setup() {
   gyro_y_cal /= 1000;                                                 
   gyro_z_cal /= 1000;                                                 
   Serial.begin(115200);
+    // start wifi and mqtt
+  WiFi.begin(ssid, pass);
+  client.begin("192.168.64.101", net);
+  client.onMessage(messageReceived);
+
+  connect();
   loop_timer = micros();                                               //Reset the loop timer
 }
 
 void loop(){
 
-  read_mpu_6050_data();   
+  // read accelerometer and gyrometer:
+  IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+  IMU.readAcceleration(acc_x, acc_y, acc_z);
  //Subtract the offset values from the raw gyro values
   gyro_x -= gyro_x_cal;                                                
   gyro_y -= gyro_y_cal;                                                
-  gyro_z -= gyro_z_cal;                                                
+  gyro_z -= gyro_z_cal;    
 
+                                              
   // 15/78 https://www.st.com/resource/en/datasheet/lsm6ds33.pdf
   // https://invensense.tdk.com/products/motion-tracking/6-axis/mpu-6050/
   // LSM6ds33 sensitivity = 70 mdps/LSB   -->  0.000004=  (1 * 10**-3 )dps * 1 / 250Hz https://stackoverflow.com/questions/19161872/meaning-of-lsb-unit-and-unit-lsb
   //Gyro angle calculations . Note 0.0000611 = 1 / (250Hz x 65.5) 
-  angle_pitch += gyro_x * 0.000004;                                   //Calculate the traveled pitch angle and add this to the angle_pitch variable
-  angle_roll += gyro_y * 0.000004;                                    //Calculate the traveled roll angle and add this to the angle_roll variable
+  angle_pitch += gyro_x * 0.004;                                   //Calculate the traveled pitch angle and add this to the angle_pitch variable
+  angle_roll += gyro_y * 0.004;                                    //Calculate the traveled roll angle and add this to the angle_roll variable
   //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
-  // 0.000000069822 = 0.000004 * (3.142 / 180)
-  angle_pitch += angle_roll * sin(gyro_z * 0.000000069822);               //If the IMU has yawed transfer the roll angle to the pitch angel
-  angle_roll -= angle_pitch * sin(gyro_z * 0.000000069822);               //If the IMU has yawed transfer the pitch angle to the roll angel
+  // 0.00000488755 = 0.004 * (3.142 / 180)
+  angle_pitch += angle_roll * sin(gyro_z * 0.00006982222);               //If the IMU has yawed transfer the roll angle to the pitch angel
+  angle_roll -= angle_pitch * sin(gyro_z * 0.00006982222);               //If the IMU has yawed transfer the pitch angle to the roll angel
   
   //Accelerometer angle calculations
   acc_total_vector = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));  //Calculate the total accelerometer vector
@@ -85,30 +137,20 @@ void loop(){
   //To dampen the pitch and roll angles a complementary filter is used
   angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;   //Take 90% of the output pitch value and add 10% of the raw pitch value
   angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;      //Take 90% of the output roll value and add 10% of the raw roll value
-  Serial.print(" | Angle  = "); Serial.println(angle_pitch_output);
+    client.loop();
+  // check if connected
+  if (!client.connected()) {
+    connect();
+  }
+  String message = String (abs(angle_roll_output)); //Serial.print(" | Angle  = "); 
+  
+  Serial.print("roll: ");
+  Serial.print((angle_roll_output));
+  Serial.print("pitch: ");
+  Serial.println((angle_pitch_output));
+  client.publish("/zGyro", message);
 
  while(micros() - loop_timer < 4000);                                 //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
  loop_timer = micros();//Reset the loop timer
   
-}
-
-
-
-
-void setup_mpu_6050_registers(){
-  //Activate the MPU-6050
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
-  Wire.write(0x6B);                                                    //Send the requested starting register
-  Wire.write(0x00);                                                    //Set the requested starting register
-  Wire.endTransmission();                                             
-  //Configure the accelerometer (+/-8g)
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
-  Wire.write(0x1C);                                                    //Send the requested starting register
-  Wire.write(0x10);                                                    //Set the requested starting register
-  Wire.endTransmission();                                             
-  //Configure the gyro (500dps full scale)
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
-  Wire.write(0x1B);                                                    //Send the requested starting register
-  Wire.write(0x08);                                                    //Set the requested starting register
-  Wire.endTransmission();                                             
 }

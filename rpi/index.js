749e09f1d6ac90dc361b12155a6d92ebc237ff9e
lukastanicic <luka.stanicic@gmail.com>
2021-04-17 16:22:39 +0200

    mqtt server + websocket server

--- rpi/index.js
+++ rpi/index.js
@@ -1,37 +1,55 @@
 const aedes = require('aedes')();
 const server = require('net').createServer(aedes.handle);
-const httpServer = require('http').createServer(handler);
+// const httpServer = require('http').createServer(handler);
+let express = require('express');
+let app = express();
+app.use('/', express.static('public'));
+
+let http = require('http');
+let server_http = http.createServer(app);
+let port_http = process.env.PORT || 3000;
+server_http.listen(port_http, function () {
+  console.log('Websocket srvr listening on port:', port_http);
+});
+let io = require('socket.io')(server_http); //require socket.io module and pass the http object (server)
+
 var nedb = require('nedb');
-var db = new nedb('mqtt.db');
-var io = require('socket.io')(httpServer); //require socket.io module and pass the http object (server)
+var db = new nedb({ filename: 'users.db', autoload: true });
 var fs = require('fs'); //require filesystem module
 const ws = require('websocket-stream');
 const port = 1883;
-const wsPort = 8080;
 
 server.listen(port, function () {
   console.log('Aedes listening on port:', port);
 });
+// This is an example
 
-ws.createServer({ server: httpServer }, aedes.handle);
+// io.sockets.on('connection', function (socket) {
+//   console.log('Connected');
+//   // WebSocket Connection
+//   // var lightvalue = 0; //static variable for current status
+//   // socket.on('gyro', function (data) {
+//   //   console.log('gyroo');
+//   //   socket.emit('gyro', 'packet.payload.toString()'); //send button status to client
+//   // });
+//   io.sockets.emit('gyro', packet.payload.toString());
+// });
 
-httpServer.listen(wsPort, function () {
-  console.log('websocket server listening on port ', wsPort);
-});
+// ws.createServer({ server: httpServer }, aedes.handle);
 
-function handler(req, res) {
-  //create server
-  fs.readFile(__dirname + '/public/index.html', function (err, data) {
-    //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-      return res.end('404 Not Found');
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-}
+// function handler(req, res) {
+//   //create server
+//   fs.readFile(__dirname + '/public/index.html', function (err, data) {
+//     //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+//       return res.end('404 Not Found');
+//     }
+//     res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
+//     res.write(data); //write data from index.html
+//     return res.end();
+//   });
+// }
 
 aedes.on('subscribe', function (subscriptions, client) {
   db.insert({
@@ -50,25 +68,12 @@ aedes.on('subscribe', function (subscriptions, client) {
   );
 });
 
-// This is an example
-
-// io.sockets.on('connection', function (socket) {
-//   // WebSocket Connection
-//   var lightvalue = 0; //static variable for current status
-//   socket.on('light', function (data) {
-//     //get light switch status from client
-//     lightvalue = data;
-//     if (lightvalue) {
-//       console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
-//     }
-//   });
-// });
 aedes.on('unsubscribe', function (subscriptions, client) {
   db.insert({
     topic: '/',
     action: 'unsubscribe',
     timestamp: new Date(),
-    message: client.id + ' ' + topic,
+    message: client.id + ' ' + subscriptions,
   });
   console.log(
     'MQTT client \x1b[32m' +
@@ -126,7 +131,6 @@ aedes.on('publish', async function (packet, client) {
     timestamp: new Date(),
     message: client.id + ' ' + packet.topic,
   });
-  //socket.emit('light', packet.payload.toString()); //send button status to client
   console.log(
     'Client \x1b[31m' +
       (client ? client.id : 'BROKER_' + aedes.id) +
@@ -137,4 +141,14 @@ aedes.on('publish', async function (packet, client) {
     'to broker',
     aedes.id
   );
+  io.sockets.on('connection', function (socket) {
+    console.log('Connected');
+    // WebSocket Connection
+    // var lightvalue = 0; //static variable for current status
+    // socket.on('gyro', function (data) {
+    //   console.log('gyroo');
+    //   socket.emit('gyro', 'packet.payload.toString()'); //send button status to client
+    // });
+    io.sockets.emit('gyro', packet.payload.toString());
+  });
 });

819fce3412ad44936adfb06422b59bec9624781c
Dorian Janezic <dtj5179@nyu.edu>
2021-04-21 17:16:41 +0200

    mapped ultrasonic sensor data to filter

--- rpi/public/app.js
+++ rpi/public/app.js
@@ -12,7 +12,10 @@ const client = mqtt.connect('ws://localhost:8888', {
     });
 
 //tone.js sampler    
-const player = new Tone.Player("sounds/diva.wav").toDestination();
+const player = new Tone.Player("sounds/diva.wav")
+
+const filter = new Tone.Filter(400, "lowpass").toDestination();
+player.connect(filter);
 
 //attach a click listener to a play button
 document.getElementById("button").addEventListener("click", async () => {
@@ -21,14 +24,19 @@ document.getElementById("button").addEventListener("click", async () => {
 });
 
 //map range
-function mapNumber (number, inMin, inMax, outMin, outMax) {
+function mapNumber (number, inMin, inMax, outMin, outMax)
+{
   return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
 //on received message from mqtt
     client.on('message', function (topic, message) {
+
+      if (message < 30) {
       console.log(topic + ': ' + message.toString());
       document.getElementById("p1").innerHTML = message.toString();
+      }
+      filter.frequency.value = mapNumber (message, 0, 30, 0, 500);
     });
 
     document.getElementById('button').addEventListener('click', function () {

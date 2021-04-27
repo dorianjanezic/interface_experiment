ccaff6774150b083eb159f49a4905950bb1dd1f4
Dorian Janezic <dtj5179@nyu.edu>
2021-04-27 20:29:52 +0200

    notes

--- rpi/public/app.js
+++ rpi/public/app.js
@@ -1,5 +1,18 @@
 //global variables
 let number = 100;
+var twostrings = 1;
+
+notes = ["C", "D", "E", "F", "G", "A", "H"]
+
+
+function calculateNote (valueString) {
+  let iterval = parseInt(valueString)% 7;
+  return (notes[iterval]);
+}
+function calculateOctave (valueString) {
+  let iterval = Math.floor(parseInt(valueString)/ 7);
+  return (iterval.toString());
+}
 
 //establishing mqtt connection over websocket port
 const client = mqtt.connect('ws://localhost:8888', {
@@ -17,6 +30,20 @@ const player = new Tone.Player("sounds/diva.wav")
 const filter = new Tone.Filter(400, "lowpass").toDestination();
 player.connect(filter);
 
+//membrane synth
+  const synth = new Tone.MetalSynth().toDestination();
+  const loop = new Tone.Loop(
+    function(time) {
+    synth.triggerAttackRelease(twostrings);
+  }, "8n").start(0);
+
+  Tone.Transport.start();
+ 
+ //play synth when button1 is pressed
+  document.getElementById("button1").addEventListener("click", function() {
+    playSynth();
+  });
+
 //attach a click listener to a play button
 document.getElementById("button").addEventListener("click", async () => {
   console.log("audio is ready");
@@ -31,14 +58,16 @@ function mapNumber (number, inMin, inMax, outMin, outMax)
 
 //on received message from mqtt
     client.on('message', function (topic, message) {
+      console.log(message.toString())
 
-      if (message < 30) {
-      console.log(topic + ': ' + message.toString());
       document.getElementById("p1").innerHTML = message.toString();
-      }
+if (message < 30) {
+      twostrings = calculateNote(message).concat(calculateOctave(message))
+      console.log(twostrings);
+}
       filter.frequency.value = mapNumber (message, 0, 30, 0, 500);
     });
-
+    
     document.getElementById('button').addEventListener('click', function () {
       client.publish('hello', 'world');
     });

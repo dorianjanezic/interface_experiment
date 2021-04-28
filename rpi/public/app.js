f9e6e0274cd31362f8553ad6a6f0a36631f9cd3e
Dorian Janezic <dtj5179@nyu.edu>
2021-04-28 19:37:08 +0200

    synth dropdown

--- rpi/public/app.js
+++ rpi/public/app.js
@@ -4,6 +4,119 @@ var twostrings = 1;
 
 notes = ["C", "D", "E", "F", "G", "A", "H"]
 
+let synths = [
+  "FM Synth",
+  "AM Synth",
+  "Membrane Synth",
+  "Plucky Synth",
+  "Metal Synth",
+  "Mono Synth",
+];
+
+let am, fm, membrane, pluck, metal, mono;
+let chosenSynth;
+
+document.querySelector("button1")?.addEventListener("click", async () => {
+  init();
+});
+
+window.addEventListener("load", () => {
+  let dropdown = document.getElementById("dropdown");
+  let defaultoption = document.createElement("option");
+  defaultoption.text = "Select Synth";
+
+  dropdown.add(defaultoption);
+
+  for (let i = 0; i < synths.length; i++) {
+    let synthOption = document.createElement("option");
+    synthOption.text = synths[i];
+    dropdown.add(synthOption);
+  }
+
+  dropdown.selectedIndex = 0;
+
+  dropdown.addEventListener("change", function (e) {
+    if (e.target.value == "FM Synth") {
+      fm = true;
+    }
+
+    if (e.target.value == "AM Synth") {
+      am = true;
+    }
+
+    if (e.target.value == "Membrane Synth") {
+      membrane = true;
+    }
+
+    if (e.target.value == "Plucky Synth") {
+      pluck = true;
+    }
+
+    if (e.target.value == "Metal Synth") {
+      metal = true;
+    }
+
+    if (e.target.value == "Mono Synth") {
+      mono = true;
+    }
+  });
+});
+
+let synthInstruments = [];
+
+function init() {
+
+  const fmSynth = new Tone.FMSynth().toDestination();
+
+  const amSynth = new Tone.AMSynth().toDestination();
+
+  const membraneSynth = new Tone.MembraneSynth().toDestination();
+
+  const pluckSynth = new Tone.PluckSynth().toDestination();
+
+  const metalSynth = new Tone.MetalSynth().toDestination();
+
+  const monoSynth = new Tone.MetalSynth().toDestination();
+
+  synthInstruments.push(
+    amSynth,
+    fmSynth,
+    membraneSynth,
+    pluckSynth,
+    metalSynth,
+    monoSynth
+  );
+
+  if (fm == true) {
+    chosenSynth = synthInstruments[0];
+  }
+
+  if (am == true) {
+    chosenSynth = synthInstruments[1];
+  }
+
+  if (membrane == true) {
+    chosenSynth = synthInstruments[2];
+  }
+
+  if (pluck == true) {
+    chosenSynth = synthInstruments[3];
+  }
+
+  if (metal == true) {
+    chosenSynth = synthInstruments[4];
+  }
+
+  if (mono == true) {
+    chosenSynth = synthInstruments[5];
+  }
+  const loop = new Tone.Loop(
+    function(time) {
+    chosenSynth.triggerAttackRelease(twostrings);
+  }, "8n").start(0);
+
+  Tone.Transport.start();
+};
 
 function calculateNote (valueString) {
   let iterval = parseInt(valueString)% 7;
@@ -31,18 +144,13 @@ const filter = new Tone.Filter(400, "lowpass").toDestination();
 player.connect(filter);
 
 //membrane synth
-  const synth = new Tone.MetalSynth().toDestination();
-  const loop = new Tone.Loop(
-    function(time) {
-    synth.triggerAttackRelease(twostrings);
-  }, "8n").start(0);
+const synth = new Tone.MetalSynth().toDestination();
+const loop = new Tone.Loop(
+  function(time) {
+  synth.triggerAttackRelease(twostrings);
+}, "8n").start(0);
 
-  Tone.Transport.start();
- 
- //play synth when button1 is pressed
-  document.getElementById("button1").addEventListener("click", function() {
-    playSynth();
-  });
+Tone.Transport.start();
 
 //attach a click listener to a play button
 document.getElementById("button").addEventListener("click", async () => {
@@ -59,15 +167,11 @@ function mapNumber (number, inMin, inMax, outMin, outMax)
 //on received message from mqtt
     client.on('message', function (topic, message) {
       console.log(message.toString())
-
       document.getElementById("p1").innerHTML = message.toString();
-if (message < 30) {
-      twostrings = calculateNote(message).concat(calculateOctave(message))
-      console.log(twostrings);
-}
+
+        if (message < 30) {
+          twostrings = calculateNote(message).concat(calculateOctave(message))
+          console.log(twostrings);
+        }
       filter.frequency.value = mapNumber (message, 0, 30, 0, 500);
-    });
-    
-    document.getElementById('button').addEventListener('click', function () {
-      client.publish('hello', 'world');
     });

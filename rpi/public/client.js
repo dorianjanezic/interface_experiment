15703c180fa67ed6f0921ad0d65efcd3d0d181fc
lukastanicic <luka.stanicic@gmail.com>
2021-05-03 12:05:18 +0200

    test

--- rpi/public/client.js
+++ rpi/public/client.js
@@ -2,17 +2,16 @@
 <!-- https://github.com/websockets/ws/blob/master/doc/ws.md#event-headers-->
 <!--https://github.com/Schmavery/facebook-chat-api/blob/e60686c725ee253ee87da40bed03b233c576ccd9/src/listenMqtt.js#L34-->
 mqtt = require('mqtt')
+<!--https://github.com/Schmavery/facebook-chat-api/blob/e60686c725ee253ee87da40bed03b233c576ccd9/src/listenMqtt.js#L34-->
 const client = mqtt.connect('wss://192.168.64.104:8888', {
-    clientId: 'javascript'
-  });
-
-//   wsOptions: {
-//     port: 8888,
-//     headers: {
-//       'user-agent': 'Chrome'
-//     }
-//   },
-// });
+  clientId: 'javascrippt',
+  wsOptions: {
+    port: 8888,
+    headers: {
+      'user-agent': 'Chrome'
+    }
+  },
+});
 console.log('client', client)
 
 
